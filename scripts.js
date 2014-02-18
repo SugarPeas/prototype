e65ad59004e25e4c92e033d9c2f928751747054a
Lance Laughlin <lancealaughlin@gmail.com>
2014-02-17 19:47:04 -0500

    Basic panoramic scrolling is working with left and right arrow keys

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
-function StartMove() {
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+        //left arrow
+        moveLeft();
+    }
+    else if (e.keyCode == '39') {
+        // right arrow
+        moveRight();
+    }
+}
+
+function init() {
 	var cssBGImage=new Image();
 	cssBGImage.src="city.jpg";
 
 	window.cssMaxWidth=cssBGImage.width;
 	window.cssXPos=0;
-	setInterval("HorzMoveBackGround()",50);
 }
 
-function HorzMoveBackGround () {
-	window.cssXPos=window.cssXPos+2;
+function moveRight () {
+	window.cssXPos=window.cssXPos-15;
+
+	toMove=document.getElementById("container");
+	toMove.style.backgroundPosition=window.cssXPos+"px 0px";
+
+}
+
+
+function moveLeft () {
+	window.cssXPos=window.cssXPos+15;
 
 	toMove=document.getElementById("container");
 	toMove.style.backgroundPosition=window.cssXPos+"px 0px";
